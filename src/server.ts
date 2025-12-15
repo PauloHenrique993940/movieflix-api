e0b7d91ed4fa945cf93bd2cd33e0157611cfbe5d
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2025-12-15 20:15:12 -0300

    Configuração Eslint inicial

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,6 @@
-/*
-const http = require('http');
+const teste = "Teste";
+console.log(teste)
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.end('Home pageo');
-  } else if (req.url === '/sobre') {
-    res.statusCode = 200;
-    res.end('About page');
-  }
-});
-
-server.listen(3000, () => {
-  console.log(`Servidor em execução em http://localhost:3000/`);
-});
-*/
+let nome: string = "Paulo";
+nome = "Renato"
+console.log(nome);
