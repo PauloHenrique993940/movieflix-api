9f635a7f761d0ab7ac687d457e5a9ad4b7ef3167
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2025-12-16 18:44:17 -0300

    Configuracao inicial prisma

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import express from 'express'
-import { PrismaClient } from './generated/prisma'
 
-const app = express() // ✅ Express
-const prisma = new PrismaClient() // ✅ Prisma
-
-app.use(express.json())
-
-app.get('/movies', async (req, res) => {
-    const movies = await prisma.movie.findMany()
-    res.json(movies)
+const port = 3000
+const app = express()
+//get, post, put, patch, delete
+app.get('/movies', (req, res) => {
+  res.send('Listagem de Filmes')
 })
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
+app.listen(port, () => {
+  console.log(`Servidor em execução em http://localhost:${port}`)
 })
