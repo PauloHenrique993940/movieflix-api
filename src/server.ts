374eefd61408a10a9b4b23c913b5848803225338
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2025-12-16 22:20:12 -0300

    Apos seguir instução do video erro persiste

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
+import { PrismaClient } from '../generated/prisma/client'
 import express from 'express'
-import { PrismaClient } from '@prisma/client'
 
-const port = 3000;
-const app = express();
-const prisma = new PrismaClient();
-//get, post, put, patch, delete
+const prisma = new PrismaClient()
+
+const app = express()
+const port = 3000
+
 app.get('/movies', async (req, res) => {
-  const movies = await prisma.movies.findMany();
-  res.json(prisma)
-});
+  const movies = await prisma.movie.findMany()
+  res.json(movies)
+})
 
 app.listen(port, () => {
   console.log(`Servidor em execução em http://localhost:${port}`)
-});
+})
