17a2d971add7d7fe09f9c7dfec9d27f0fae62726
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2026-04-11 13:18:13 -0300

    adiciona a rota de listagem de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
 import express from 'express';
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/prisma/client.js';
+
 const port = 3000;
 const app = express();
 
-// GET, POST, PUT, PATCH DELETE
-app.get('/movies', (req, res) => {
-  res.send('Listagem de filmes');
+// Configurando a conexão direta com o PostgreSQL (via Adapter)
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+// Agora passamos o 'adapter' no construtor do PrismaClient
+const prisma = new PrismaClient({ adapter });
+
+// Rota para buscar todos os filmes
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await prisma.movie.findMany();
+    res.json(movies);
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao buscar filmes no banco de dados' });
+  }
 });
 
 app.listen(port, () => {
